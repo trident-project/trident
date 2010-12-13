0d1c1f96e62ed6a65978d2015b41858802b12722
Britton Smith <brittonsmith@gmail.com>
2010-12-13 12:13:43 -0500

    Changed __init__ imports to not be dumb.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,11 @@
-from SD93IonBalance import *
-from CloudyIonBalance import *
+from SD93IonBalance import \
+    add_SD93_ion_fraction_field, \
+    add_SD93_ion_number_density_field, \
+    add_SD93_ion_density_field, \
+    add_SD93_ion_mass_field
+from CloudyIonBalance import \
+    add_Cloudy_ion_fraction_field, \
+    add_Cloudy_ion_number_density_field, \
+    add_Cloudy_ion_density_field, \
+    add_Cloudy_ion_mass_field
+
