26d151062bc63a447433e4ab31bdcd03043e91a1
Cameron Hummels <chummels@gmail.com>
2015-01-20 16:45:19 -0700

    Adding __init__.py file so that trident acts as a module and has multiple imports in its namespace (including the modified_ion_balance module).

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+Trident is a python-based package extending yt for generating synthetic
+observations of hydrodynamical datasets.
+
+Please visit the Trident and yt websites for more information:
+http://trident-project.org
+http://yt-project.org
+"""
+
+from modified_ion_balance.cloudy_ion_balance import \
+    add_Cloudy_ion_number_density_field 
+
+from trident import *
