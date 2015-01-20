4080ebc11a7b5aaac74cf1f3db1f14ceb90aad93
Cameron Hummels <chummels@gmail.com>
2015-01-19 18:11:49 -0700

    Moving spectrum_plot functionality to trident.py

--- trident.py
+++ trident.py
@@ -5,6 +5,7 @@ import os
 from yt.analysis_modules.absorption_spectrum.api import \
       AbsorptionSpectrum
 from yt.funcs import mylog
+from matplotlib import pyplot
       
 atomic_mass = {'H': 1.00794, 'He': 4.002602, 'Li': 6.941,
                'Be': 9.012182, 'B': 10.811, 'C': 12.0107,
@@ -118,7 +119,7 @@ class SpectrumGenerator(AbsorptionSpectrum):
                 element = element[:1]        
 
             if "Ly" in list_ion:
-                field = "%s_number_density" % ion
+                field = "H_number_density" 
             else:
                 field = "%s_Cloudy_eq_NumberDensity_post" % ion
                 
@@ -167,3 +168,51 @@ class SpectrumGenerator(AbsorptionSpectrum):
         output.create_dataset('flux', data=self.flux_field)
         output.create_dataset('rootflux', data=self.flux_field**0.5)
         output.close()
+
+def plot_spectrum(wavelength, flux, filename):
+    # number of rows and columns
+    n_rows = 1
+    n_columns = 1
+
+    # blank space between edge of figure and active plot area
+    top_buffer = 0.05
+    bottom_buffer = 0.15
+    left_buffer = 0.05
+    right_buffer = 0.03
+    
+    # blank space between plots
+    hor_buffer = 0.05
+    vert_buffer = 0.05
+
+    # calculate the height and width of each panel
+    panel_width = ((1.0 - left_buffer - right_buffer - 
+                    ((n_columns-1)*hor_buffer)) / n_columns)
+    panel_height = ((1.0 - top_buffer - bottom_buffer - 
+                     ((n_rows-1)*vert_buffer)) / n_rows)
+
+    # create a figure (figsize is in inches)
+    pyplot.figure(figsize=(12, 4))
+
+    # loop over all panels
+    for i in range(n_rows * n_columns):
+
+        # get the row and column number
+        my_row = i / n_columns
+        my_column = i % n_columns
+
+        # calculate the position of the bottom, left corner of this plot
+        left_side = left_buffer + (my_column * panel_width) + \
+          my_column * hor_buffer
+        top_side = 1.0 - (top_buffer + (my_row * panel_height) + \
+                          my_row * vert_buffer)
+        bottom_side = top_side - panel_height
+
+        # create an axes object on which we will make the plot
+        my_axes = pyplot.axes((left_side, bottom_side, panel_width, panel_height))
+
+        my_axes.plot(wavelength, flux)
+        my_axes.xaxis.set_label_text("$\\lambda$ [$\\AA$]")
+        my_axes.yaxis.set_label_text("relative flux")
+        my_axes.set_xlim(wavelength.min(), wavelength.max())
+        my_axes.set_ylim(0, flux.max()*1.1)
+        pyplot.savefig(filename)
