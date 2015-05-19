0507fc13808a673e3839845ed160781663710e28
Devin Silvia <devin.silvia@gmail.com>
2015-05-19 12:55:25 -0500

    updating __init__.py to match the new format and single ion_balance file.

--- ion_balance/__init__.py
+++ ion_balance/__init__.py
@@ -1,12 +1,4 @@
-from roman import \
-     fromRoman, \
-     toRoman
-from SD93IonBalance import \
-    add_SD93_ion_fraction_field, \
-    add_SD93_ion_number_density_field, \
-    add_SD93_ion_density_field, \
-    add_SD93_ion_mass_field
-from CloudyIonBalance import \
+from ion_balance import \
     add_ion_fraction_field, \
     add_ion_number_density_field, \
     add_ion_density_field, \
