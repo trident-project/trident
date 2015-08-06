240970a8e25adcc7e544810e7749a377f1cb34f8
Devin Silvia <devin.silvia@gmail.com>
2015-08-06 20:58:51 +0100

    Removined modified_ion_balance because it is no longer needed.

--- modified_ion_balance/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Trident is a python-based package extending yt for generating synthetic
-observations of hydrodynamical datasets.
-
-Please visit the Trident and yt websites for more information:
-http://trident-project.org
-http://yt-project.org
-"""
