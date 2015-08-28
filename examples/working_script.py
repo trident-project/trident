2240e51f3f962fb3b463467546c3a04b9ab51927
Cameron Hummels <chummels@gmail.com>
2015-08-28 14:06:50 -0700

    Updating working_script to now work with tip of trident and tip of ytdata bookmark.

--- examples/working_script.py
+++ examples/working_script.py
@@ -1,39 +1,40 @@
-# Example of a currently working script using trident to generate a COS 
+# Example of a currently working script using trident to generate a COS
 # spectrum from a non-cosmological dataset
 
+import os
+import yt
 import trident as tri
 
-# Load the dataset and define the coordinates of the start/end of the ray
+# Define the dataset and the coordinates of the start/end of the ray
 fn = 'enzo_cosmology_plus/RD0009/RD0009'
-fn = 'enzo_cosmology_plus/AMRCosmology.enzo'
 ray_start = [0,0,0]
 ray_end = [1,1,1]
 
-# Make a yt LightRay object for the temperature, density, and metallicity 
-# fields of our dataset at redshift_start = redshift_end = 0.0.  Include true 
-# HI to trump over Cloudy estimation of HI.  Save LightRay to ray.h5 and 
-# ray.txt and use it internally as "ray"
-lr = tri.LightRay(fn, 'Enzo', 0.0, 0.2)
-ray = lr.make_light_ray(seed=12345, start_position=ray_start, end_position=ray_end,
-                  fields=[('gas', 'temperature'), 
-                          ('gas', 'density'), 
-                          ('gas', 'H_number_density'), 
-                          ('gas', 'metallicity')],
-                  solution_filename="ray.txt", data_filename="ray.h5",
-                  get_los_velocity=True)
+# Make a yt LightRay object for the temperature, density, and metallicity
+# fields of our dataset at redshift_start = redshift_end = 0.0.  Include true
+# HI to override the Cloudy estimation of HI.  Save LightRay to ray.h5 and
+# ray.txt and use it internally as "ray".  If it already exists, then load it
+# as a dataset.
 
-ray = tri.add_ion_fields_to_ray(ray, save_filename="mod_ray.h5")
+output_file = "ray.h5"
+if os.path.exists(output_file):
+    ray = yt.load(output_file)
+else:
+    lr = tri.LightRay(fn)
+    ray = lr.make_light_ray(start_position=ray_start, end_position=ray_end, 
+                solution_filename="ray.txt", data_filename=output_file,
+                fields=['temperature', 'density', 'H_number_density', 
+                'metallicity'])
 
-# Now use the mod_ray h5 file to actually generate an absorption spectrum
+# Now use the ray object to actually generate an absorption spectrum
+# Use the settings (spectral range, LSF, and spectral resolution for COS)
 sg = tri.SpectrumGenerator('COS')
-sg.make_spectrum("mod_ray.h5", output_file="spec.h5",
-                line_list_file="line_list.txt")
-#sg.make_flat_spectrum()
+sg.make_spectrum(ray, output_file="spec.h5", line_list_file=None)
 
-# "Final" spectrum with added quasar, MW background, and gaussian noise (SNR=30)
-sg.add_qso_spectrum(redshift=0.2)
+# "Final" spectrum with added quasar, MW background, and gaussian noise 
+# (SNR=30)
+sg.add_qso_spectrum(redshift=0.0)
 sg.add_milky_way_foreground()
 sg.apply_lsf()
-sg.add_gaussian_noise(10)
-#sg.flux_field.clip(0, sg.flux_field.max(), out=sg.flux_field)
+sg.add_gaussian_noise(30)
 tri.plot_spectrum(sg.lambda_bins, sg.flux_field, 'spec.png')
