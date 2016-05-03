c39798ae4e63d4e95e7aa0671e564e7e83442be9
Cameron Hummels <chummels@gmail.com>
2016-05-02 21:33:04 -0700

    Updating top level imports.

--- trident/__init__.py
+++ trident/__init__.py
@@ -19,10 +19,6 @@ __version__ = "0.4"
 
 from trident.ion_balance import \
     add_ion_fields, \
-    add_ion_fraction_field, \
-    add_ion_number_density_field, \
-    add_ion_density_field, \
-    add_ion_mass_field, \
     solar_abundance, \
     atomic_mass
 
