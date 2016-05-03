b064efa49bcaf2bc1c34eac38124140cc07eec17
Cameron Hummels <chummels@gmail.com>
2016-05-02 22:34:34 -0700

    When ion_balance has a bad interpolation with > 1 ion-fraction, cap it at 1 and provide output warning.

--- trident/ion_balance.py
+++ trident/ion_balance.py
@@ -649,8 +649,8 @@ def _ion_fraction_field(field,data):
     fraction[fraction <= fraction_zero_point] = 0.0
     if not isinstance(data, FieldDetector) and (fraction > 1.0).any():
         greater_than = fraction > 1.0
-        mylog.warning("An ion fraction greater than 1 was calculated. Bad interpolation: " +
-                      "capping at 1.0")
+        mylog.warning("An ion fraction greater than 1 was calculated. ")
+        mylog.warning("Bad interpolation: capping at 1. ")
         mylog.warning("Original values: %s" % fraction[greater_than])
     return fraction
 
