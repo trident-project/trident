0d74f00894f66389c8a277c8c7762d55216abed3
Cameron Hummels <chummels@gmail.com>
2016-05-19 09:59:23 -0700

    Updating working script example

--- examples/working_script.py
+++ examples/working_script.py
@@ -1,36 +1,38 @@
 # Example of a currently working script using trident to generate a COS
-# spectrum from a non-cosmological dataset
+# spectrum from an Enzo dataset
 
-import os
-import trident as tri
+import yt
+import trident
 
-# Define the dataset and the coordinates of the start/end of the ray
+# Set the dataset filename, load it into yt and define the trajectory
+# of the LightRay
 fn = 'enzo_cosmology_plus/RD0009/RD0009'
-ray_start = [0,0,0]
-ray_end = [1,1,1]
+ds = yt.load(fn)
+ray_start = ds.domain_left_edge
+ray_end = ds.domain_right_edge
 
-# Make a LightRay object for the temperature, density, and metallicity
-# fields of our dataset at redshift_start = redshift_end = 0.0.  Include 
-# HI field calculated in simulation to override the Cloudy estimation of HI.  
+# Make a LightRay object including the temperature, density, and metallicity
+# fields of our dataset. Include HI field calculated in simulation to 
+# override the ion_balance estimation of HI.  
 # Save LightRay to ray.h5 and use it locally as ray object.
-ray = tri.make_simple_ray(fn, start_position=ray_start,
-                          end_position=ray_end, data_filename='ray.h5',
-                          fields=['density', 'temperature', 'metallicity',
-                          'H_p0_number_density'])
+ray = trident.make_simple_ray(ds, start_position=ray_start,
+                              end_position=ray_end, data_filename='ray.h5',
+                              fields=['density', 'temperature', 'metallicity',
+                              'H_p0_number_density'])
 
 # Now use the ray object to actually generate an absorption spectrum
 # Use the settings (spectral range, LSF, and spectral resolution) for COS
-# And save it as an output hdf5 file and plot it to an image.
-sg = tri.SpectrumGenerator('COS')
+# And save it as an output text file and plot it to an image.
+sg = trident.SpectrumGenerator('COS')
 sg.make_spectrum(ray)
-sg.save_spectrum('spec_raw.h5')
+sg.save_spectrum('spec_raw.txt')
 sg.plot_spectrum('spec_raw.png')
 
-# "Final" spectrum with added quasar, MW background, and gaussian noise 
-# (SNR=30)
+# "Final" spectrum with added quasar, MW background, applied line-spread 
+# function, and added gaussian noise (SNR=30)
 sg.add_qso_spectrum()
 sg.add_milky_way_foreground()
 sg.apply_lsf()
 sg.add_gaussian_noise(30)
-sg.save_spectrum('spec_final.h5')
+sg.save_spectrum('spec_final.txt')
 sg.plot_spectrum('spec_final.png')
