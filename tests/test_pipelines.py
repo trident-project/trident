966636b839a15c82522f8d79baf1936dd0804e25
Cameron Hummels <chummels@gmail.com>
2016-05-18 21:37:23 -0700

    Adding test_pipelines now that I know that drone.io will work.

---
+++ tests/test_pipelines.py
@@ -0,0 +1,22 @@
+"""
+Tests to assure the code runs on example pipelines
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016, Trident Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from trident.utilities import verify
+
+def test_verify():
+    """
+    Test that the verify function works and the code can create a dataset,
+    generate a LightRay and then construct a spectrum from it.
+    """
+    verify()
+    assert True
