6453ecddce80840aaedba123ce0d48a976c18f6a
Cameron Hummels <chummels@gmail.com>
2016-07-30 11:34:27 -0700

    Adding tests for saving and loading spectra of different formats.

---
+++ tests/test_spectrum_generator.py
@@ -0,0 +1,71 @@
+"""
+Tests for Spectrum Generation
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016, Trident Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from trident.utilities import make_onezone_ray
+from trident.spectrum_generator import \
+    SpectrumGenerator, \
+    load_spectrum
+import tempfile
+import shutil
+import os
+
+def test_save_load_spectrum_ascii():
+    """
+    Test that we can save and load spectra in the ASCII format
+    """
+    dirpath = tempfile.mkdtemp()
+    filename = os.path.join(dirpath, 'ray.h5')
+    ray = make_onezone_ray(column_densities={'H_p0_number_density':1e21}, 
+                                   filename=filename)
+    sg = SpectrumGenerator(lambda_min=1200, lambda_max=1300, dlambda=0.5)
+    sg.make_spectrum(ray, lines=['Ly a'])
+    sg.save_spectrum(os.path.join(dirpath, 'spec.txt'))
+    del(sg)
+    sg = load_spectrum(os.path.join(dirpath, 'spec.txt'))
+    sg.plot_spectrum(filename=os.path.join(dirpath, 'spec.png'))
+    shutil.rmtree(dirpath)
+    assert True
+
+def test_save_load_spectrum_hdf5():
+    """
+    Test that we can save and load spectra in the HDF5 format
+    """
+    dirpath = tempfile.mkdtemp()
+    filename = os.path.join(dirpath, 'ray.h5')
+    ray = make_onezone_ray(column_densities={'H_p0_number_density':1e21}, 
+                                   filename=filename)
+    sg = SpectrumGenerator(lambda_min=1200, lambda_max=1300, dlambda=0.5)
+    sg.make_spectrum(ray, lines=['Ly a'])
+    sg.save_spectrum(os.path.join(dirpath, 'spec.h5'))
+    del(sg)
+    sg = load_spectrum(os.path.join(dirpath, 'spec.h5'))
+    sg.plot_spectrum(filename=os.path.join(dirpath, 'spec.png'))
+    shutil.rmtree(dirpath)
+    assert True
+
+def test_save_load_spectrum_fits():
+    """
+    Test that we can save and load spectra in the FITS format
+    """
+    dirpath = tempfile.mkdtemp()
+    filename = os.path.join(dirpath, 'ray.h5')
+    ray = make_onezone_ray(column_densities={'H_p0_number_density':1e21}, 
+                                   filename=filename)
+    sg = SpectrumGenerator(lambda_min=1200, lambda_max=1300, dlambda=0.5)
+    sg.make_spectrum(ray, lines=['Ly a'])
+    sg.save_spectrum(os.path.join(dirpath, 'spec.fits'))
+    del(sg)
+    sg = load_spectrum(os.path.join(dirpath, 'spec.fits'))
+    sg.plot_spectrum(filename=os.path.join(dirpath, 'spec.png'))
+    shutil.rmtree(dirpath)
+    assert True
