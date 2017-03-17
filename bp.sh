c11b2f46bd62755a8df3eda8652326354d8e34b0
Britton Smith <brittonsmith@gmail.com>
2017-03-17 16:05:12 -0700

    Adding bitbucket pipelines testing files.

---
+++ bp.sh
@@ -0,0 +1,30 @@
+# install conda and yt dependencies with conda
+cd ..
+wget --quiet https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash ./Miniconda2-latest-Linux-x86_64.sh -b -p ./trident-conda -f
+export PATH=$PWD/trident-conda/bin:$PATH
+conda install -q -y mercurial cython h5py matplotlib sympy numpy pytest flake8
+
+# set up development build of yt
+hg clone https://bitbucket.org/yt_analysis/yt yt-hg
+cd yt-hg
+hg up yt
+pip install -e .
+cd ..
+
+# repo has been cloned here
+cd $BITBUCKET_CLONE_DIR
+hg up tip
+pip install -e .
+
+# install data file and create config.tri file in CWD
+# so trident works, despite not having access to ~/.trident
+cd tests
+wget --quiet http://trident-project.org/data/ion_table/config.tri
+wget --quiet http://trident-project.org/data/ion_table/hm2012_lr.h5.gz
+gunzip hm2012_lr.h5.gz
+wget --quiet http://trident-project.org/data/tests/enzo_small.tar.gz
+tar -zxvf enzo_small.tar.gz
+
+# start the tests themselves
+py.test tests
