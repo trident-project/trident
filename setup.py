38ef537f1e14b65a9c12ed0314db642ffcffea69
Britton Smith <brittonsmith@gmail.com>
2017-05-03 20:45:14 -0700

    Increasing version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "trident",
-    version = "1.0.1",
+    version = "1.1.0.dev1",
     author = "Cameron Hummels, Devin Silvia, Britton Smith",
     author_email = "trident-project-users@googlegroups.com",
     description = ("Spectrum generator for astrophysical simulation data"),
