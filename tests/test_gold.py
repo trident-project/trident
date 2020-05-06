560583771c1f9bcd0a038b45cbc626da08825325
Cameron Hummels <chummels@gmail.com>
2020-05-06 15:45:43 -0700

    Adding test for gold standard code.

---
+++ tests/test_gold.py
@@ -0,0 +1,19 @@
+"""
+Unit test for download_file function
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, Trident Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import os
+
+from gold_standard_versions import \
+    get_gold_standard_version
+
+def test_gold_standard_versions():
+    get_gold_standard_version()
