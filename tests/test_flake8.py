a16fa75d8a79108992a86072f5f76be63cab6729
Britton Smith <brittonsmith@gmail.com>
2020-06-06 14:18:50 +0100

    Use correct directory for flake8 test.

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -2,16 +2,17 @@ import subprocess
 import os
 
 def test_flake8():
-    python_trident_dir = os.path.dirname(
+    package_dir = os.path.dirname(
         os.path.dirname(os.path.abspath(__file__)))
+    source_dir = os.path.join(package_dir, 'trident')
     output_file = os.environ.get("WORKSPACE", None) or os.getcwd()
     output_file = os.path.join(output_file, 'flake8.out')
     if os.path.exists(output_file):
         os.remove(output_file)
     output_string = "--output-file=%s" % output_file
     config_string = "--config=%s" % os.path.join(
-        python_trident_dir, 'setup.cfg')
-    subprocess.call(['flake8', output_string, config_string, python_trident_dir])
+        package_dir, 'setup.cfg')
+    subprocess.call(['flake8', output_string, config_string, source_dir])
 
     with open(output_file) as f:
         flake8_output = f.readlines()
