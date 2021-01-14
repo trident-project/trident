aa46bfba005bc31e50727f348febda9bc1fdb9b5
Cameron Hummels <chummels@gmail.com>
2021-01-14 10:55:24 -0800

    Switching sphinx to 3.2.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ import glob
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '3.4.3'
+needs_sphinx = '3.2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
