9255f39da918883541758c6f61450a16e84509d5
Cameron Hummels <chummels@gmail.com>
2022-08-22 11:57:24 -0700

    Removing support for python 3.6

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -66,5 +65,5 @@ setup(
         'scipy',
         'yt>=4.0.1',
         'yt_astro_analysis'],
-      python_requires='>=3.6'
+      python_requires='>=3.7'
 )
