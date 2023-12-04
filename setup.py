92d04f4cf6935d233efe20dd2036eeb5424cb2e3
Cameron Hummels <chummels@gmail.com>
2023-12-03 22:15:19 -0800

    Updating setup.py with python reqs

--- setup.py
+++ setup.py
@@ -44,9 +44,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Astronomy",
         "Topic :: Scientific/Engineering :: Physics",
         "Topic :: Scientific/Engineering :: Visualization",
@@ -63,5 +64,5 @@ setup(
         'scipy',
         'yt>=4.0.1',
         'yt_astro_analysis>=1.1.0'],
-      python_requires='>=3.7'
+      python_requires='>=3.9'
 )
